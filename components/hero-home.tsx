7027042df265c3032c855af4d53a8fd1033b4cc0
arneym3 <arneym3@gmail.com>
2026-01-29 19:14:30 +0500

    Update hero-home.tsx

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -16,7 +16,7 @@ export default function Hero() {
               </p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="mailto:твой-email@pixsence.com">Request a pilot</a>
+                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="mailto:arneym3@gmail.com">Request a pilot</a>
                 </div>
                 <div>
                   <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Get the deck</a>
