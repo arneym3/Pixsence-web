ba6424c9f6336cfb97403a8efe3132108377d5c1
arneym3 <arneym3@gmail.com>
2026-01-30 10:12:25 +0500

    Update page.tsx

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,7 @@
 import Hero from '../../components/hero'
 import Features from '../../components/features-planet'
-import Newsletter from '../../components/newsletter'
+// Добавляем /ui/ в путь, так как файл лежит там
+import Newsletter from '../../components/ui/newsletter' 
 
 export const metadata = {
   title: 'Pixsence - Modular on-prem Risk Platform',
