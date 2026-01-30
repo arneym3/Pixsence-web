489c64a1f79c6074c7a7349252cdc3febd4f8614
arneym3 <arneym3@gmail.com>
2026-01-30 13:35:32 +0500

    Update features-planet.tsx

--- components/features-planet.tsx
+++ components/features-planet.tsx
@@ -1,10 +1,6 @@
 import Image from "next/image";
 import PlanetImg from "@/public/images/planet.png";
 import PlanetOverlayImg from "@/public/images/planet-overlay.svg";
-import PlanetTagImg01 from "@/public/images/planet-tag-01.png"; 
-import PlanetTagImg02 from "@/public/images/planet-tag-02.png";
-import PlanetTagImg03 from "@/public/images/planet-tag-03.png";
-import PlanetTagImg04 from "@/public/images/planet-tag-04.png";
 
 export default function FeaturesPlanet() {
   return (
@@ -20,19 +16,13 @@ export default function FeaturesPlanet() {
             </p>
           </div>
 
-          {/* Planet Section (Signals) */}
+          {/* Planet Section - Clean version without tags */}
           <div className="pb-16 md:pb-20" data-aos="zoom-y-out">
             <div className="text-center">
               <div className="relative inline-flex rounded-full before:absolute before:inset-0 before:-z-10 before:scale-[.85] before:animate-[pulse_4s_cubic-bezier(.4,0,.6,1)_infinite] before:bg-linear-to-b before:from-blue-900 before:to-sky-700/50 before:blur-3xl after:absolute after:inset-0 after:rounded-[inherit] after:[background:radial-gradient(closest-side,var(--color-blue-500),transparent)]">
                 <Image className="rounded-full bg-gray-900" src={PlanetImg} width={400} height={400} alt="Pixsence Core" />
                 <div className="pointer-events-none" aria-hidden="true">
                   <Image className="absolute -right-64 -top-20 z-10 max-w-none" src={PlanetOverlayImg} width={789} height={755} alt="Network" />
-                  <div>
-                    <Image className="absolute -left-28 top-16 z-10 animate-[float_4s_ease-in-out_infinite_both] opacity-80" src={PlanetTagImg01} width={253} height={56} alt="Power Signal" />
-                    <Image className="absolute left-56 top-7 z-10 animate-[float_4s_ease-in-out_infinite_1s_both] opacity-30" src={PlanetTagImg02} width={241} height={56} alt="Telemetry" />
-                    <Image className="absolute -left-20 bottom-24 z-10 animate-[float_4s_ease-in-out_infinite_2s_both] opacity-25" src={PlanetTagImg03} width={243} height={56} alt="Audit Logs" />
-                    <Image className="absolute bottom-32 left-64 z-10 animate-[float_4s_ease-in-out_infinite_3s_both] opacity-80" src={PlanetTagImg04} width={251} height={56} alt="Visual Data" />
-                  </div>
                 </div>
               </div>
             </div>
